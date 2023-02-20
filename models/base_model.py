0fb470b0941574a88edf079afbc2a8dbde5d6040
Soniabensaad <5687@holbertonstudents.com>
2023-02-20 15:56:26 +0100

    airbnb project

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+"""Defines a class Base"""
+class BaseModel:
+    """ defines all common attributes/
+    methods for other classes"""
+    def __init__(self, name="", my_number=0):
+            self.name = name
+            self.my_number = my_number
+            self.__class__ = BaseModel
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
+        
+
+
+
+    def __str__(self):
+        return f"[{BaseModel}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict["class"] = self.__class__.__name__
+        new_dict["created_at"] = str(datetime.datetime.now())
+        new_dict["updated_at"] = str(datetime.datetime.now())
+        return new_dict
+
+
---
+++ tests/test_base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+class testBase_AirBnb(unittest.TestCase):
+    def test_base_to_dict(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+        self.assertEqual(
+            (my_model_json), "{'my_number': 89, 'name': 'My First Model', 'updated_at': '2023-02-20 15:42:59.938285', 'id': '4a7e3d5c-d604-4dc2-89e1-733ae9b7079d', 'created_at': '2023-02-20 15:42:59.938256','__class__': 'BaseModel'}")
