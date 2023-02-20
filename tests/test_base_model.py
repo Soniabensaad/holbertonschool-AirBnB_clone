2eb8be4dc5595a722885c6c60f3c5b55c52bb415
Soniabensaad <5687@holbertonstudents.com>
2023-02-20 16:06:57 +0100

    airbnb project

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -7,5 +7,5 @@ class testBase_AirBnb(unittest.TestCase):
         my_model.name = "My First Model"
         my_model.my_number = 89
         my_model_json = my_model.to_dict()
-        self.assertEqual(
+        self.assertEqual(print
             (my_model_json), "{'my_number': 89, 'name': 'My First Model', 'updated_at': '2023-02-20 15:42:59.938285', 'id': '4a7e3d5c-d604-4dc2-89e1-733ae9b7079d', 'created_at': '2023-02-20 15:42:59.938256','__class__': 'BaseModel'}")
