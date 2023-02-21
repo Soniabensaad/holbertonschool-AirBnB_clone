a15a11068a2be52114a485b5562acc1be16f92a1
Soniabensaad <5687@holbertonstudents.com>
2023-02-21 17:06:13 +0100

    airbnb project

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,16 +25,4 @@ class FileStorage:
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             f.write(json_f)
 
-    def reload(self):
-        try:
-            with open(FileStorage.__file_path, "r") as f:
-                data = json.load(f)
-                new_dict = {}
-                for key, value in data.items():
-                    class_name = key.split(".")[0]
-                    obj = eval(class_name)(**value)
-                    new_dict[key] = obj
-                    FileStorage.__objects[key] = new_dict
-        except FileNotFoundError:
-            pass
-
+   
