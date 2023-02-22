3812966370bdfcadb7d755a7c31a92dd88cc62b7
Soniabensaad <5687@holbertonstudents.com>
2023-02-22 23:14:27 +0100

    air bnb project

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,13 @@
+import unittest
+from models.city import City
+from models.base_model import BaseModel
+class TestUser(unittest.TestCase):
+    """tests for city class"""
+    def test_city_id(self):
+        """test id  city"""
+        self.assertIsInstance(self.state_id, str)
+
+    def test_city_name(self):
+        """test name city"""
+        self.assertIsInstance(self.name, str)
+
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,8 +1,8 @@
 import unittest
-from models.user import User
+from models.state import State
 from models.base_model import BaseModel
 class TestUser(unittest.TestCase):
     """tests for state class"""
     def test_state(self):
-        """test for email user"""
+        """test for name state"""
         self.assertIsInstance(self.state.name, str)
