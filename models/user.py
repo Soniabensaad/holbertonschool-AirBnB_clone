2ec2376c3e7af54c40fd75d35109c525550883c3
Soniabensaad <5687@holbertonstudents.com>
2023-02-22 22:27:19 +0100

    air bnb project

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""User Class.
+This module contains a class that inherits from the BaseModel class."""
+
+from models.base_model import BaseModel
+class User(BaseModel):
+    """class User that inherits from BaseModel
+          Public class attributes:
+          email: string - empty string
+          password: string - empty string
+          first_name: string - empty string
+          last_name: string - empty string
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
