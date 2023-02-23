612094153d43ac31380b3ab3a00a7e7f83f88957
Soniabensaad <5687@holbertonstudents.com>
2023-02-23 18:30:51 +0100

    air bnb project

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""new class to store data"""
 import json
 import os
 from models.base_model import BaseModel
@@ -8,25 +9,25 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-
-
 class FileStorage:
-   
-
+    """ serializes instances to a JSON file and
+    deserializes JSON file to instances"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """returns the dictionary __objects"""
         return (self.__objects)
 
     def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
         obj_name = obj.__class__.__name__
         obj_id = obj.id
         key = f"{obj_name}.{obj_id}"
         self.__objects[key] = obj
 
     def save(self):
-
+        """serializes __objects to the JSON file"""
         dictionnary = {}
         for key in self.__objects:
             dictionnary[key] = self.__objects[key].to_dict()
@@ -34,6 +35,7 @@ class FileStorage:
             json.dump(dictionnary, f)
 
     def reload(self):
+        """deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, 'r') as f:
                 data = json.load(f)
@@ -44,4 +46,3 @@ class FileStorage:
                     self.__objects[key] = obj
         except BaseException:
             pass
-
