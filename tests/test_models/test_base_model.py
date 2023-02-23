b27dbb8ec89b3b994845ee127aa031d4a8baa511
Soniabensaad <5687@holbertonstudents.com>
2023-02-23 17:33:36 +0100

    air bnb project

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,6 +15,6 @@ class testBase_AirBnb(unittest.TestCase):
             (my_model_json), "{'my_number': 89, 'name': 'My First Model', 'updated_at': '2023-02-20 14:24:01.871493', 'id': '853d2737-0bc7-4dbe-a403-b3934a3de25c', 'created_at': '2023-02-20 14:24:01.871499', '__class__': 'BaseModel'}")
     def test_assert(self):
         my_model = BaseModel()
-        assert(my_model.save(), my_model.save())
-        assert(my_model.id, my_model.id)
-        assert(my_model.to_dict(), my_model.to_dict())
+        assert(my_model.save()== my_model.save())
+        assert(my_model.id == my_model.id)
+        assert(my_model.to_dict() == my_model.to_dict())
