fcb035a1eccb1a425751ee9d659caf4f23964ed7
Soniabensaad <5687@holbertonstudents.com>
2023-02-23 13:57:11 +0100

    air bnb project

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -3,11 +3,10 @@ from models.city import City
 from models.base_model import BaseModel
 class TestCity(unittest.TestCase):
     """tests for city class"""
-    def test_city_id(self):
-        """test id  city"""
-        self.assertIsInstance(self.state_id, str)
-
-    def test_city_name(self):
-        """test name city"""
-        self.assertIsInstance(self.name, str)
+    def test_city(self):
+        """test for  city"""
+        my_city1 = City()
+        my_city1.name = "New York"
+        my_city1.state_id = "9bf17966-b092-4996-bd33-26a5353cccb4"
+        
 
