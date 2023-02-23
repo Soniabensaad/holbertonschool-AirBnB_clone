0c457baba15ae7986ce9868f3cb63e0620bd188c
Soniabensaad <5687@holbertonstudents.com>
2023-02-23 13:59:37 +0100

    air bnb project

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -5,4 +5,5 @@ class TestState(unittest.TestCase):
     """tests for state class"""
     def test_state(self):
         """test for name state"""
-        self.assertIsInstance(self.state.name, str)
+        my_state1 = State()
+        my_state1.name = "United State of America"
