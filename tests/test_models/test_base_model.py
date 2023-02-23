31bb157f30878519dc7feeac978a5fc6d9b1ce9e
Soniabensaad <5687@holbertonstudents.com>
2023-02-23 16:13:14 +0100

    air bnb project

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,9 +8,14 @@ class testBase_AirBnb(unittest.TestCase):
         my_model = BaseModel()
         my_model.name = "My First Model"
         my_model.my_number = 89
+        my_model = "{'my_number': 89, 'name': 'My First Model', 'updated_at': '2023-02-20 14:24:01.871493', 'id': '853d2737-0bc7-4dbe-a403-b3934a3de25c', 'created_at': '2023-02-20 14:24:01.871499', '__class__': 'BaseModel'}")
+
         my_model_json = my_model.to_dict()
         self.assertEqual(
-            (my_model_json), "{'my_number': 89, 'name': 'My First Model', 'updated_at': '2023-02-20 15:42:59.938285', 'id': '4a7e3d5c-d604-4dc2-89e1-733ae9b7079d', 'created_at': '2023-02-20 15:42:59.938256', '__class__': 'BaseModel'}")
+            (my_model_json), "{'my_number': 89, 'name': 'My First Model', 'updated_at': '2023-02-20 14:24:01.871493', 'id': '853d2737-0bc7-4dbe-a403-b3934a3de25c', 'created_at': '2023-02-20 14:24:01.871499', '__class__': 'BaseModel'}")
+
+    
+
         self.aassertEqual(my_model.save(), my_model.save())
         self.aassertEqual(my_model.id, my_model.id)
         self.aassertEqual(my_model.to_dict(), my_model.to_dict())
