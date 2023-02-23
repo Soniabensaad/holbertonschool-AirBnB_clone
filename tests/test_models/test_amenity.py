868fbd8eb7d5db27dd8b89a1077220cca60e63bf
Soniabensaad <5687@holbertonstudents.com>
2023-02-23 13:12:17 +0100

    air bnb project

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -3,6 +3,12 @@ from models.amenity import Amenity
 from models.base_model import BaseModel
 class TestCity(unittest.TestCase):
     """test for amenity"""
-    def test_city_name(self):
+    def test_amenity_name(self):
         """test name amenity"""
         self.assertIsInstance(self.name, str)
+
+    def test_name_attr(self):
+        """Test that Amenity has attribute name, and it's as an empty string"""
+        amenity = Amenity()
+        self.assertTrue(hasattr(amenity, "name"))
+        self.assertEqual(amenity.name, "")
